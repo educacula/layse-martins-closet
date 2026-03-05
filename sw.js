5cf252b520c83d9dc97d0ee406b4a7da42d03ba7
educacula <educacula@gmail.com>
2026-03-05 11:30:37 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "layse-martins-closet-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./logo.png","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((res) => {
+        if (event.request.method === "GET" && res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        }
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
